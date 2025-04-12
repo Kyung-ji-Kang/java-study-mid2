bdecde0ac6f4f8b3f3a819d0b85762bfaa8e51e2
Kyung-ji-Kang <kangkj001@gmail.com>
2025-04-12 23:15:23 +0900

    20250412

---
+++ src/collection/set/test/UniqueNamesTest1.java
@@ -0,0 +1,24 @@
+package collection.set.test;
+
+import java.util.HashSet;
+import java.util.Set;
+
+public class UniqueNamesTest1 {
+
+
+    public static void main(String[] args) {
+        Integer[] inputArr = {30, 20, 20, 10, 10};
+
+        Set<Integer> set  = new HashSet<>();
+
+        for(int num: inputArr){
+            set.add(num);
+        }
+
+        for(Integer num:set){
+            System.out.println(num);
+        }
+
+
+    }
+}
